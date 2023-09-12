b062ba6175a200229f854513d565776e246b8c48
PavelKarshankov <domiwenokk@icloud.com>
2023-09-12 13:52:50 +0300

    Main commit

---
+++ scripts/utils/constants.js
@@ -0,0 +1,3 @@
+const backgroundImage = document.querySelector('.map');
+const markers = backgroundImage.querySelector('.map__markers');
+const templateBtn = document.querySelector('.btn-template').content;
